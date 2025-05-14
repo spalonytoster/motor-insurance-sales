07ef09f19054878f337845d66b586aa9a15dfbcc
spalonytoster <xmachiei@gmail.com>
2025-05-14 18:32:47 +0200

    Initial commit - empty project

---
+++ src/main/java/com/example/motorinsurancesales/MotorInsuranceSalesApplication.java
@@ -0,0 +1,13 @@
+package com.example.motorinsurancesales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotorInsuranceSalesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MotorInsuranceSalesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/motorinsurancesales/MotorInsuranceSalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.motorinsurancesales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotorInsuranceSalesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
